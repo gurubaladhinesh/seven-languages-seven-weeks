7cce0ba7d6fb59c2072500ad49f863f77586ec4d
gurubaladhinesh <gurubaladhinesh@gmail.com>
2022-09-21 17:52:11 +0530

    intial commit

---
+++ ruby/day1/homework.rb
@@ -0,0 +1,13 @@
+puts 'Hello, world'
+
+puts "Hello, Ruby,".index("Ruby")
+
+def print_ten_times
+    x=1
+    until x>10 do
+        puts "Guru Bala Dhinesh #{x}" 
+        x = x+1 
+    end
+end
+
+print_ten_times
